90af82273712955aa2f6919058c264fb88dd52d4
Sunghayo <notsungha@gmail.com>
2025-07-30 02:59:19 +0800

    Create  firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDnusZ7fck0nU_b0iDZAbAUyliQ0Dbc5Mc",
+  authDomain: "mycleanjcu.firebaseapp.com",
+  projectId: "mycleanjcu",
+  messagingSenderId: "137404600804",
+  appId: "1:137404600804:web:e2bd6b28fe6d9b7cb637f1"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log("📩 Background message: ", payload);
+  const { title, body } = payload.notification;
+
+  self.registration.showNotification(title, {
+    body,
+    icon: "/icons/icon-192x192.png" // need to change Icon 
+  });
+});
