117a46d00f21124f2d2de183fb13324839acb11b
Sunghayo <notsungha@gmail.com>
2025-07-30 03:43:28 +0800

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,18 +5,19 @@ firebase.initializeApp({
   apiKey: "AIzaSyDnusZ7fck0nU_b0iDZAbAUyliQ0Dbc5Mc",
   authDomain: "mycleanjcu.firebaseapp.com",
   projectId: "mycleanjcu",
+  storageBucket: "mycleanjcu.appspot.com",
   messagingSenderId: "137404600804",
   appId: "1:137404600804:web:e2bd6b28fe6d9b7cb637f1"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-  console.log("📩 Background message: ", payload);
-  const { title, body } = payload.notification;
+messaging.onBackgroundMessage((payload) => {
+  console.log("📦 Received background message: ", payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body
+  };
 
-  self.registration.showNotification(title, {
-    body,
-    icon: "/icons/icon-192x192.png" // need to change Icon 
-  });
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
