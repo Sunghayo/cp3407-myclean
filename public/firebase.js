f93d7e0b533704a01cfcbc19ef5c15414b88d940
Sunghayo <notsungha@gmail.com>
2025-07-30 04:24:22 +0800

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,8 +1,7 @@
-// firebase.js
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDnusZ7fck0nU_b0iDZAbAUyliQ0Dbc5Mc",
@@ -13,10 +12,10 @@ const firebaseConfig = {
   appId: "1:137404600804:web:e2bd6b28fe6d9b7cb637f1"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
 const db = getFirestore(app);
 const auth = getAuth(app);
-
 const messaging = getMessaging(app);
 
 const VAPID_KEY = "BDUIz_uBYrL6sgMb2igVA0mkc7VAmH2qaJexVTQjz2MbhLf8brSMVlbWBTnlvXyvBACYXwCXPwbhOG5I98Rc3g8";
